b05eb7c8dc5e8518e1e161a68dc6a12a91599b43
yonasbekele166 <yonasbekele166@gmail.com>
2023-03-16 15:26:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
-#!/bin/bash
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include <studio.h>
 
+
+/**
+ * main - prints "Programming is like building a multilingual 
+ * puzzle, followed by a newline."
+ * return 0.
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
