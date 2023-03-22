15da32939b8b76f31fc9374da78eea8b0ab4706a
yonasbekele166 <yonasbekele166@gmail.com>
2023-03-22 14:10:59 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,12 +11,11 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a > b && b > c)
-	
 	{
 		largest = a;
-       	}
+	}
 	else if (b > a && a > c)
 	{
 		largest = b;
@@ -25,6 +24,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
 	return (largest);
 }
